d6a92e2cc0d39094ca58e241a0184807a93a559d
NguyenVanSang-coder <62025815+NguyenVanSang-coder@users.noreply.github.com>
2022-06-06 16:37:40 +0700

    first commit

---
+++ src/app/controllers/HomeController.js
@@ -0,0 +1,22 @@
+import res from 'express/lib/response';
+
+class HomeController {
+  index(req, res) {
+    res.render('home');
+  }
+
+  // trang con trong trang home
+  /*
+    GET /home/:slug
+  */
+  show(req, res) {
+    res.send('home detail!');
+  }
+}
+
+// module.exports = new HomeController;
+
+// export default HomeController;
+const home = new HomeController();
+export { home };
+//export {new HomeController() }
